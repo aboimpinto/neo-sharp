21b0f6d5cecac88935cb692c85d06815767f09d4
Paulo Aboim Pinto <aboimpinto@gmail.com>
2018-08-14 02:13:21 +0200

    Add test class, #301

---
+++ test/NeoSharp.Core.Test/Blockchain/Processors/UtBlockPool.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NeoSharp.TestHelpers;
+
+namespace NeoSharp.Core.Test.Blockchain.Processors
+{
+    [TestClass]
+    public class UtBlockPool : TestBase
+    {
+    }
+}
