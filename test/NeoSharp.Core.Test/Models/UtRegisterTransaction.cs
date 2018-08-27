4634eda5a2f4cd94505ab63ec33ebd07eb34acba
Paulo Aboim Pinto <aboimpinto@gmail.com>
2018-08-27 16:53:57 +0200

    Tests sign the Utility Token register transaction

--- test/NeoSharp.Core.Test/Models/UtRegisterTransaction.cs
+++ test/NeoSharp.Core.Test/Models/UtRegisterTransaction.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NeoSharp.BinarySerialization;
@@ -60,6 +61,43 @@ namespace NeoSharp.Core.Test.Models
             signedRegisterTransaction.Hash.ToString(true)
                 .Should()
                 .Be("0xc56f33fc6ecfcd0c225c4ab356fee59390af8560be0e930faebe74a6daff7c9b");
+        }
+
+        [TestMethod]
+        public void Sign_ProvideUtilityToken_SignedTypeReturnedWithTheRightHash()
+        {
+            const uint decrementInterval = 2000000;
+            uint[] gasGenerationPerBlock = { 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+
+            BinarySerializer.RegisterTypes(typeof(RegisterTransaction).Assembly);
+
+            var unsignedRegisterTransaction = new RegisterTransaction
+            {
+                AssetType = AssetType.UtilityToken,
+                Name = "[{\"lang\":\"zh-CN\",\"name\":\"小蚁币\"},{\"lang\":\"en\",\"name\":\"AntCoin\"}]",
+                Amount = Fixed8.FromDecimal(gasGenerationPerBlock.Sum(p => p) * decrementInterval),
+                Precision = 8,
+                Owner = ECPoint.Infinity,
+                Admin = new[] { (byte)EVMOpCode.PUSH0 }.ToScriptHash(),         //TODO: Why this? Check with people
+                Attributes = new TransactionAttribute[0],
+                Inputs = new List<CoinReference>(),
+                Outputs = new List<TransactionOutput>(),
+                Witness = new List<Core.Models.Witnesses.Witness>()
+            };
+
+            var crypto = Crypto.Default;
+            var witnessSignatureManager = this.AutoMockContainer.Create<WitnessSignatureManager>();
+            var binarySerializer = this.AutoMockContainer.Create<BinarySerializer>();
+
+            var testee = new RegisterTransactionSignatureManager(crypto, witnessSignatureManager, binarySerializer);
+            var signedRegisterTransaction = testee.Sign(unsignedRegisterTransaction);
+
+            signedRegisterTransaction
+                .Should()
+                .BeOfType<SignedRegisterTransaction>();
+            signedRegisterTransaction.Hash.ToString(true)
+                .Should()
+                .Be("0x602c79718b16e442de58778e148d0b1084e3b2dffd5de6b7b16cee7969282de7");
         }
     }
 }
